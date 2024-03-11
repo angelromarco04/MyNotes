a6a61351005cd643123f6fd4badb29dd63c385f6
AngelAM <angelromarco04@gmail.com>
2024-03-11 16:35:46 +0100

    -03-2024_1635

--- push.bat
+++ push.bat
@@ -3,12 +3,12 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
 git add .
-echo ""
+echo 
 
 git commit -m %mydate%_%mytime%
-echo ""
+echo 
 
 git push
-echo ""
+echo 
 
 pause
